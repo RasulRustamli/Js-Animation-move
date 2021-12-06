d76c4d2f64f992644f5cb024781b2a9548f7a39a
RasulRustamli <rasular@code.edu.az>
2021-12-06 20:37:40 +0400

    first commit

---
+++ Js/script.js
@@ -0,0 +1,35 @@
+function init() {
+    objBox = document.getElementById("box");
+    objBox.style.left = "0px";
+    objBox.style.top = "0px";
+}
+function getKeyAndMove(e) {
+    var key_code = e.which || e.keyCode;
+    switch (key_code) {
+        case 37: 
+            moveLeft();
+            break;
+        case 38: 
+            moveUp();
+            break;
+        case 39: 
+            moveRight();
+            break;
+        case 40: 
+            moveDown();
+            break;
+    }
+}
+function moveLeft() {
+    objBox.style.left = parseInt(objBox.style.left) - 5 + "px";
+}
+function moveUp() {
+    objBox.style.top = parseInt(objBox.style.top) - 5 + "px";
+}
+function moveRight() {
+    objBox.style.left = parseInt(objBox.style.left) + 5 + "px";
+}
+function moveDown() {
+    objBox.style.top = parseInt(objBox.style.top) + 5 + "px";
+}
+window.onload = init;
